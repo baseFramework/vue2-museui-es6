e6e6d94cb54c217ee06aacb095c99291f0f4c7d5
rahul <ro87630872@126.com>
2016-12-23 13:59:14 +0800

    【UPD】完成生物列表

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <com-head></com-head>
-    <com-navi v-bind:active = "biology"></com-navi>
-    <com-datalist v-bind:items="biolist" v-bind:ends="bioend" v-on:updatelist = "updatebiolist"></com-datalist>
+    <com-navi v-bind:active="biology"></com-navi>
+    <com-datalist v-bind:items="biolist" v-bind:ends="bioend" v-on:updatelist="updatebiolist"></com-datalist>
   </div>
 </template>
 
@@ -15,38 +15,32 @@
     data () {
       return {
         msg: 'Welcome to Your Vue.js App',
-        indexMsg: '223',
-        biolist:[],
-        bioend:0
+        biolist: [],
+        bioend: 0
       }
     },
     components: {
-      comHead:head,
-      comNavi:navi,
-      comDatalist:datalist
+      comHead: head,
+      comNavi: navi,
+      comDatalist: datalist
     },
     created: function () {
       // `this` 指向 vm 实例
       this.$store.dispatch('SET_INDEXMSG', 'Hello World!223')
       this.$store.dispatch('SET_BIOLOGYLIST', 0)
     },
-    mounted: function () {
-      // `this` 指向 vm 实例
-      this.indexMsg = this.$store.getters.getIndexData;
-    },
-    computed:{
+    computed: {
       biolist(){
-        //console.log('length1122:' + this.$store.getters.getBiologyList.length);
         return this.$store.getters.getBiologyList
       },
       bioend(){
-        console.log( this.$store.getters.getBioEnd);
+        console.log(this.$store.getters.getBioEnd);
         return this.$store.getters.getBioEnd
       }
     },
-    methods:{
-      updatebiolist:function(start){
-        this.$store.dispatch('SET_BIOEND',start);
+    methods: {
+      updatebiolist: function (start) {
+        this.$store.dispatch('SET_BIOEND', start);
         this.$store.dispatch('SET_BIOLOGYLIST', start)
       }
     }
